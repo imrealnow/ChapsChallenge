3f6eee34230816f3e0b123e3abd9108b95696a98
Liam Green <imrealnow95@gmail.com>
2022-09-07 23:24:34 +1200

    Added dependencies

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/App.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/App.java
@@ -4,10 +4,8 @@ package nz.ac.vuw.ecs.swen225.gp22;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
