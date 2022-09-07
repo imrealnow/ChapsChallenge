1a1e1dfe319c9db4eefd6fa08a608012cf6d9fbf
Liam Green <imrealnow95@gmail.com>
2022-09-07 19:46:09 +1200

    Made initial maven project

---
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/App.java
@@ -0,0 +1,13 @@
+package nz.ac.vuw.ecs.swen225.gp22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
