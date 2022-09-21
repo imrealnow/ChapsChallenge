88cc4400d4673e6033cb1925103559b67aaf6fa1
Liam Green <imrealnow95@gmail.com>
2022-09-21 22:04:45 +1200

    Created XML save/loading class

---
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistence/XMLSerializer.java
@@ -0,0 +1,101 @@
+package nz.ac.vuw.ecs.swen225.gp22.persistence;
+
+import java.io.File;
+
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * Utility class for creating a file chooser for saving and loading XML files.
+ * 
+ * @author Liam Green - greenliam
+ */
+public class XMLSerializer {
+    enum FileAction {
+        SAVE, LOAD
+    }
+
+    /**
+     * Show a dialog to select a file to load, or set the default file name to save
+     * to.
+     * 
+     * @param parentComponent the parent component of the dialog
+     * @param windowTitle     the title of the dialog
+     * @param action          the action to perform
+     * @return the file to load or save to
+     */
+    public File showFileChooser(JComponent parentComponent, String windowTitle, FileAction action) {
+        // Create file chooser
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle(windowTitle);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        // Add a filter to only show/allow .xml files
+        FileFilter xmlFilter = new FileFilter() {
+            @Override
+            public boolean accept(java.io.File f) {
+                return f.isDirectory() || f.getName().toLowerCase().endsWith(".xml");
+            }
+
+            @Override
+            public String getDescription() {
+                return "XML files (*.xml)";
+            }
+        };
+        fileChooser.setFileFilter(xmlFilter);
+        // Show the file chooser as a open/save dialog
+        int result = action == FileAction.SAVE ? fileChooser.showSaveDialog(parentComponent)
+                : fileChooser.showOpenDialog(parentComponent);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            // if the user selected a file, return it
+            return fileChooser.getSelectedFile();
+        }
+        return null;
+    }
+
+    /**
+     * Opens a dialog to choose a name for a file to save to, and then saves the
+     * 
+     * @param <T>             the type of object to save
+     * @param parentComponent the parent component of the dialog
+     * @param windowTitle     the title of the dialog
+     * @param factory         the xml factory for the type of object to save
+     * @return the file that was saved
+     */
+    public <T> File saveObjectToXML(JComponent parentComponent, String windowTitle, XMLFactory<T> factory,
+            T objectToSave) {
+        String path = showFileChooser(parentComponent, windowTitle, FileAction.SAVE).getAbsolutePath();
+        if (path != null) {
+            try {
+                return factory.createXML(path, objectToSave);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Opens a dialog to choose a file to load, parses it using the factory, and
+     * then
+     * returns the object.
+     * 
+     * @param <T>             the type of object to load
+     * @param parentComponent the parent component of the dialog
+     * @param windowTitle     the title of the dialog
+     * @param factory         the xml factory for the type of object to load
+     * @return the object loaded from the file
+     */
+    public <T> T loadObjectFromXML(JComponent parentComponent, String windowTitle, XMLFactory<T> factory) {
+        File xmlFile = showFileChooser(parentComponent, windowTitle, FileAction.LOAD);
+        if (xmlFile != null) {
+            try {
+                return factory.createFromXML(xmlFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
