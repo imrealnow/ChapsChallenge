94f4b1f2eb7c8554618f1f86e2720e3480eaf518
Bradley Cave <cavebrad@myvuw.ac.nz>
2022-09-23 00:39:05 +1200

    Fixed a bug caused in Entity by making Game a singleton

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/Elements/Entity.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/Elements/Entity.java
@@ -1,6 +1,4 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain.Elements;
-
-import nz.ac.vuw.ecs.swen225.gp22.domain.Game;
 import nz.ac.vuw.ecs.swen225.gp22.util.Sprite;
 import nz.ac.vuw.ecs.swen225.gp22.util.Vector;
 
