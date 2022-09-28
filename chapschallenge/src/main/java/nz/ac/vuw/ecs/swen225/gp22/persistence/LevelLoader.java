02123e36c689fa605daf79b87b2f877e0d8142a3
Liam Green <imrealnow95@gmail.com>
2022-09-28 20:45:30 +1300

    Added a way to get or load from a level's index

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistence/LevelLoader.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistence/LevelLoader.java
@@ -1,7 +1,9 @@
 package nz.ac.vuw.ecs.swen225.gp22.persistence;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.dom4j.DocumentException;
@@ -22,7 +24,7 @@ public enum LevelLoader {
 
     private String resourcePath;
     private static LevelFactory levelFactory = new LevelFactory();
-    private static Set<Level> loadedLevels;
+    private static Map<Level, LevelLoader> loadedLevels;
 
     /**
      * Loads a predefined game level.
@@ -52,11 +54,42 @@ public enum LevelLoader {
      */
     public static Set<Level> loadAll() {
         if (loadedLevels == null) {
-            loadedLevels = new HashSet<>();
+            loadedLevels = new HashMap<>();
             for (LevelLoader levelLoader : LevelLoader.values()) {
-                loadedLevels.add(levelLoader.load());
+                loadedLevels.put(levelLoader.load(), levelLoader);
             }
         }
-        return Set.copyOf(loadedLevels);
+        return Set.copyOf(loadedLevels.keySet());
+    }
+
+    /**
+     * Gets the index of a level in the predefined levels.
+     * 
+     * @param level the level to get the index of
+     * @return the index of the level
+     */
+    public static int getLevelIndex(Level level) {
+        if (loadedLevels == null) {
+            loadAll();
+        }
+        LevelLoader levelLoader = loadedLevels.get(level);
+        if (levelLoader == null) {
+            throw new RuntimeException("Level not found");
+        }
+        return levelLoader.ordinal();
+    }
+
+    /**
+     * Loads a level using its index
+     * 
+     * @param index the index of the level to load
+     * @return the level loaded from the XML file
+     */
+    public static Level getLevel(int index) {
+        LevelLoader[] values = LevelLoader.values();
+        if (index < 0 || index >= values.length) {
+            throw new RuntimeException("Invalid level index");
+        }
+        return LevelLoader.values()[index].load();
     }
 }
