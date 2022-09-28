d8e89376913a8aeeeadc1ea2bf82dbe66c6294c5
huijsejuli <huijsejuli@ecs.vuw.ac.nz>
2022-09-28 20:23:25 +1300

    Added method to add ActionRecord to a list of actions. Fixed javadoc wording in ActionRecord

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelReplay.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelReplay.java
@@ -1,10 +1,24 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * LevelReplay saves the specific level for replay
  * 
  * @author Julia Huijser - huijsejuli
  */
 public class LevelReplay {
+
+    private List<ActionRecord> actions = new ArrayList<ActionRecord>();
+
+    /**
+     * Add ActionRecord to the list of actions
+     * 
+     * @param a
+     */
+    public void add(ActionRecord a){
+        actions.add(a);
+    }
     
 }
