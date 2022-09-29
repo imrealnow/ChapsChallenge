fe24fa796e222bacb08a1c98ec1520495491e55d
huijsejuli <huijsejuli@ecs.vuw.ac.nz>
2022-09-30 11:23:51 +1300

    got rid of unneeded imports to make it neater

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
@@ -1,26 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.SAXReader;
-import org.dom4j.io.XMLWriter;
+import java.io.IOException;
 
 import nz.ac.vuw.ecs.swen225.gp22.app.ActionController;
 import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Bindings;
-import nz.ac.vuw.ecs.swen225.gp22.persistence.XMLFactory;
-import nz.ac.vuw.ecs.swen225.gp22.persistence.XMLSerializer;
 import nz.ac.vuw.ecs.swen225.gp22.util.Time;
 import nz.ac.vuw.ecs.swen225.gp22.util.observer.Observer;
 
