176b44bf23250bd6283b6553b8af399885c34edc
huijsejuli <huijsejuli@ecs.vuw.ac.nz>
2022-09-30 11:15:05 +1300

    Javadoc for LevelReplay

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelReplay.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelReplay.java
@@ -16,12 +16,20 @@ public class LevelReplay {
     private List<ActionRecord> actions;
     private int levelIndex;
 
+    /**
+    * Level Replay Constructor
+    * 
+    * @param levelIndex the int of the levels index to know which level to replay
+    * @param actions List of the actions that took place during that play to replay
+    */
     public LevelReplay(int levelIndex, List<ActionRecord> actions) {
         this.levelIndex = Game.getInstance().getLevel().getLevelID();
         this.actions = actions;
     }
 
-    // blank constructor for making an new file
+    /**
+     * Blank Level Replay Constructor
+     */
     public LevelReplay() {
         this.levelIndex = Game.getInstance().getLevel().getLevelID();
         this.actions = new ArrayList<>();
@@ -30,16 +38,24 @@ public class LevelReplay {
     /**
      * Add ActionRecord to the list of actions
      * 
-     * @param a
+     * @param a an Action Record 
      */
     public void add(ActionRecord a) {
         actions.add(a);
     }
 
+    /**
+     * Getter for the list of ActionRecords to get the actions of the entity
+     * @return
+     */
     public Iterable<ActionRecord> getActions() {
         return actions;
     }
 
+    /**
+     * Getter for the level index to know which level we're on
+     * @return
+     */
     public int getLevelIndex() {
         return levelIndex;
     }
