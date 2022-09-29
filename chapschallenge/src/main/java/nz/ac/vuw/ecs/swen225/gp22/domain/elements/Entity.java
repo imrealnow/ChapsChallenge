e57940c42fde90fc1c1c9c5df0d2cdaf7918d22c
Liam Green <imrealnow95@gmail.com>
2022-09-30 01:10:18 +1300

    Refactoring: synchronised actions to update loop

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/elements/Entity.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/elements/Entity.java
@@ -2,6 +2,11 @@ package nz.ac.vuw.ecs.swen225.gp22.domain.elements;
 
 import nz.ac.vuw.ecs.swen225.gp22.util.Sprite;
 import nz.ac.vuw.ecs.swen225.gp22.util.Vector;
+
+import java.util.List;
+import java.util.Optional;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.Game;
 import nz.ac.vuw.ecs.swen225.gp22.util.Direction;
 
 /**
@@ -9,9 +14,18 @@ import nz.ac.vuw.ecs.swen225.gp22.util.Direction;
  */
 public abstract class Entity implements Updateable {
     private Vector position;
+    private Optional<Runnable> queuedAction = Optional.empty();
 
     public Entity(Vector position) {
         this.position = position;
+    }
+
+    @Override
+    public void update() {
+        if (queuedAction.isPresent()) {
+            queuedAction.get().run();
+            queuedAction = Optional.empty();
+        }
     }
 
     public Vector getPosition() {
@@ -22,7 +36,39 @@ public abstract class Entity implements Updateable {
         this.position = newPosition;
     }
 
-    public abstract boolean tryMove(Direction dir);
+    public boolean tryMove(Direction dir) {
+        Vector newPos = getPosition().add(dir.vector());
+
+        Tile[][] tiles = Game.getInstance().getLevel().getTiles();
+        int levelHeight = tiles.length;
+        int levelWidth = tiles[0].length;
+
+        // Case 0: The player is moving to a invalid positon
+        if (newPos.x() < 0 || newPos.x() >= levelWidth || newPos.y() < 0 || newPos.y() >= levelHeight) {
+            throw new IndexOutOfBoundsException("Player tried to move out of bounds");
+        }
+
+        List<Interactable> interactables = Game.getInstance().getInteractablesAt(newPos);
+
+        // if any interactables block movement, return false
+        if (interactables.stream().anyMatch(i -> !i.entityCanStep(this))) {
+            // can't move, so don't apply move
+            // applyMove(dir, interactables);
+            return false;
+        }
+
+        // otherwise, move the player
+        applyMove(dir, interactables);
+        return true;
+
+    }
+
+    protected void applyMove(Direction dir, List<Interactable> interactables) {
+        queuedAction = Optional.of(() -> {
+            setPosition(getPosition().add(dir.vector()));
+            interactables.forEach(i -> i.onInteract(this));
+        });
+    }
 
     public Sprite getSprite() {
         throw new UnsupportedOperationException();
--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/objects/entities/Player.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/domain/objects/entities/Player.java
@@ -21,12 +21,10 @@ public class Player extends Entity {
         this.facing = Direction.Down;
     }
 
-    /**
-     * This method should be called every frame, taking inputs and updating the
-     * player accordingly.
-     */
+    // runs queued command
     public void update() {
-
+        super.update();
+        // do anything else you need, but always call super.update() first
     }
 
     public HashMap<Item, Integer> inventory() {
@@ -51,36 +49,9 @@ public class Player extends Entity {
         return Sprite.LegendDown;
     }
 
-    public boolean tryMove(Direction dir) {
-
-        Vector newPos = getPosition().add(dir.vector());
-
-        Tile[][] tiles = Game.getInstance().getLevel().getTiles();
-        int levelHeight = tiles.length;
-        int levelWidth = tiles[0].length;
-
-        // Case 0: The player is moving to a invalid positon
-        if (newPos.x() < 0 || newPos.x() >= levelWidth || newPos.y() < 0 || newPos.y() >= levelHeight) {
-            throw new IndexOutOfBoundsException("Player tried to move out of bounds");
-        }
-
-        List<Interactable> interactables = Game.getInstance().getInteractablesAt(newPos);
-
-        // if any interactables block movement, return false
-        if (interactables.stream().anyMatch(i -> !i.entityCanStep(this))) {
-            // can't move, so don't apply move
-            // applyMove(dir, interactables);
-            return false;
-        }
-
-        // otherwise, move the player
-        applyMove(dir, interactables);
-        return true;
-    }
-
-    private void applyMove(Direction dir, List<Interactable> interactables) {
-        interactables.stream().forEach(i -> i.onInteract(this));
-        setPosition(getPosition().add(dir.vector()));
+    @Override
+    protected void applyMove(Direction dir, List<Interactable> interactables) {
+        super.applyMove(dir, interactables);
         facing = dir;
     }
 }
