26a00996d6bb375d48bb898d90d480e1d93573d8
huijsejuli <huijsejuli@ecs.vuw.ac.nz>
2022-09-30 11:22:53 +1300

    Javadoc for LevelRecorder

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
@@ -24,24 +24,36 @@ import nz.ac.vuw.ecs.swen225.gp22.persistence.XMLSerializer;
 import nz.ac.vuw.ecs.swen225.gp22.util.Time;
 import nz.ac.vuw.ecs.swen225.gp22.util.observer.Observer;
 
+/**
+ * LevelRecorder records the level and saves the current replay of that level to an
+ * XML file
+ * 
+ * @author Julia Huijser - huijsejuli
+ */
 public class LevelRecorder implements Observer<ActionController, Bindings> {
     private static final LevelReplayFactory recordingFactory = new LevelReplayFactory();
 
     private LevelReplay currentLevelInProgress = new LevelReplay();
 
     /**
-     * TOOD: Add Javadoc
+     * LevelRecorder constructor 
      */
     public LevelRecorder() {
         App.INSTANCE.getController().addObserver(this);
     }
 
+    /**
+     * Notify notifies the observer of changes in the subject
+     */
     @Override
     public void notify(ActionController t, Bindings r) {
         ActionRecord action = new ActionRecord(r, Time.INSTANCE.getTimeSinceStart());
         currentLevelInProgress.add(action);
     }
 
+    /**
+     * SaveCurrentReplay saves the current replay into an XML file
+     */
     public void saveCurrentReplay(String path) throws IOException {
         try {
             recordingFactory.createXML(path, currentLevelInProgress);
