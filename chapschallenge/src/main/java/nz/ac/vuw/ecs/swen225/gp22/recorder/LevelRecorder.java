73ddda5ac1d742344c7d331c21ebe8e8a1dc1292
huijsejuli <huijsejuli@ecs.vuw.ac.nz>
2022-09-29 20:45:56 +1300

    setup() method as well as getting rid of int time in recordPlayerAction

--- chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
+++ chapschallenge/src/main/java/nz/ac/vuw/ecs/swen225/gp22/recorder/LevelRecorder.java
@@ -1,15 +1,30 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
+import nz.ac.vuw.ecs.swen225.gp22.app.App;
 import nz.ac.vuw.ecs.swen225.gp22.app.Bindings;
 
 public class LevelRecorder {
 
     private LevelReplay currentLevelInProgress = new LevelReplay();
 
-    public ActionRecord recordPlayerAction(Bindings input, int time){
-        ActionRecord action = new ActionRecord(input, time);
+    /**
+     * TODO: Add Javadoc
+     * 
+     * @param input
+     * @param time
+     * @return
+     */
+    public ActionRecord recordPlayerAction(Bindings input){
+        ActionRecord action = new ActionRecord(input);
         currentLevelInProgress.add(action);
         return null;
+    }
+
+    /**
+     * TODO: Javadoc
+     */
+    public void setup(){
+        App.INSTANCE.getController().addObserver((controller, action) -> recordPlayerAction(action));
     }
     
 }
